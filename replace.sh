9cd87659270ddf5712afe02df4c65d179788dba7
medoangweb <goctiengphap@gmail.com>
2021-12-25 14:33:24 +0100

    update

--- replace.sh
+++ replace.sh
@@ -1,3 +1,5 @@
-find ./ -type f ! -name "*.sh" -exec sed -i -e 's/http:\/\//https:\/\//g' {} \; 
-sleep 1
-find ./ -type f ! -name "*.sh" -exec sed -i -e 's/192.168.0.*\//medoang.com\//g' {} \;
+find ./ -type f ! -name "*.sh" -exec sed -i -e 's/http:\/\//https:\/\//g' {} \;
+sleep 5
+find ./ -type f ! -name "*.sh" -exec sed -i -e 's/192.168.0.[[:digit:]][[:digit:]]\//medoang.com\//g' {} \;
+sleep 5
+find ./ -type f ! -name "*.sh" -exec sed -i -e 's/medoang.official/HangPhapBonjour/g' {} \;
--- wp-content/themes/kale/assets/js/jquery.smartmenus.js
+++ wp-content/themes/kale/assets/js/jquery.smartmenus.js
@@ -1,9 +1,9 @@
 /*!
  * SmartMenus jQuery Plugin - v1.1.0 - September 17, 2017
- * http://www.smartmenus.org/
+ * https://www.smartmenus.org/
  *
  * Copyright Vasil Dinkov, Vadikom Web Ltd.
- * http://vadikom.com
+ * https://vadikom.com
  *
  * Licensed MIT
  */
